d98f6f43d618640316165b5055ef5736dfca3830
DenisS <den_sorokin_1@mail.ru>
2019-04-07 18:02:27 +0300

    Ready

--- index.php
+++ index.php
@@ -11,9 +11,7 @@ error_reporting(E_ALL);
     <title>table</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <link href="css/style.css" rel="stylesheet">
-    <script src="js/jquery-3.3.1.js"></script>
-    <script src="js/bootstrap.js"></script>
-    <script src="js/custom.js"></script>
+
 </head>
 <body>
 <pre>
