76ab7f35a7694de44071df8a064fe604998a3e9a
DenisS <den_sorokin_1@mail.ru>
2019-04-16 22:54:40 +0300

    Task manager v1.1

--- lk.php
+++ lk.php
@@ -3,7 +3,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 //header('location: index.php');
-$login = isset($_POST['login']) ? $_POST['login'] : '';
+$loginMod = isset($_POST['login']) ? $_POST['login'] : '';
+$oldPass = isset($_POST['oldPass']) ? $_POST['oldPass'] : '';
+$newPass = isset($_POST['newPass']) ? $_POST['newPass'] : '';
+$newRepeatPass = isset($_POST['newRepeatPass']) ? $_POST['newRepeatPass'] : '';
 
 
 $db = mysqli_connect('site.local',
@@ -16,11 +19,9 @@ if (!$db) {
     print_r($db->error_list);
     //exit();
 }
-$query='';
+$query = '';
 $query = "SELECT `id`,`login`,`password`,`user_name` FROM users";
 $res = mysqli_query($db, $query);
-
-
 
 
 //$row = mysqli_fetch_assoc($res);
@@ -29,6 +30,28 @@ while ($row = mysqli_fetch_assoc($res)) {
     $users[] = $row;
 
 
+}
+echo '<pre>';
+print_r($users);
+
+$username = $users[2]['user_name'];
+
+
+echo $username;
+
+foreach ($users as $user) {
+
+
+    $logPass = password_verify($oldPass, $user['password']);
+    $newHashPass = password_hash($newRepeatPass,PASSWORD_DEFAULT);
+
+    if ($loginMod == $user['login'] && $logPass == true) {
+        $tempUser=$user['login'];
+        $queryMod = "UPDATE `users` SET `password`='$newHashPass'WHERE `login` ='$loginMod'  ";//UPDATE `users` SET `login`='www' WHERE (`id`='71')
+        $result = mysqli_query($db,$queryMod);
+        mysqli_close($db);
+        break 1;
+    }
 }
 
 
@@ -47,15 +70,17 @@ while ($row = mysqli_fetch_assoc($res)) {
 <body>
 
 <div class="form-row align-items-center formDivLogin">
-    <?= '<pre>' ?>
+    <pre>
     <form action="lk.php" method="post">
-        <input class="form-control" placeholder="Введите старый пароль" name="oldPass">
-        <input class="form-control" placeholder="Введите новый пароль" type="password" autocomplete="off" name="newPass">
-        <input class="form-control" placeholder="Повторите пароль" type="password" autocomplete="off" name="newRepeatPass">
-<!--        <div>-->
-<!--            <input class="form-check-input" type="checkbox" name="cbRemember">Remember me-->
-<!---->
-<!--        </div>-->
+        <input class="form-control" placeholder="Enter login" name="loginMyAcc">
+        <input class="form-control" placeholder="Enter old password" autocomplete="off" name="oldPass">
+        <input class="form-control" placeholder="Enter new password" type="password" autocomplete="off" name="newPass">
+        <input class="form-control" placeholder="Repeat new password" type="password" autocomplete="off"
+               name="newRepeatPass">
+        <!--        <div>-->
+        <!--            <input class="form-check-input" type="checkbox" name="cbRemember">Remember me-->
+        <!---->
+        <!--        </div>-->
         <button class="btn btn-primary" type="submit">Login</button>
 
     </form>
--- signup.php
+++ signup.php
@@ -17,7 +17,7 @@ $signPass = isset($_POST['signPass']) ? $_POST['signPass'] : '';
 $signName = isset($_POST['signName']) ? $_POST['signName'] : '';
 
 ///////////////////////////////////////////////////////////////////////////////////////
-if(!empty($signLogin) && !empty($signPass) && !empty($signName) ) {
+if (!empty($signLogin) && !empty($signPass) && !empty($signName)) {
     $db = mysqli_connect('site.local',
         'root',
         '',
@@ -29,6 +29,62 @@ if(!empty($signLogin) && !empty($signPass) && !empty($signName) ) {
         exit();
     }
 
+
+    $query = "SELECT `id`,`login` FROM users";
+    $res = mysqli_query($db, $query);
+
+
+//$row = mysqli_fetch_assoc($res);
+    $signLoginCheck = [];
+    while ($row = mysqli_fetch_assoc($res)) {
+        $signLoginCheck[] = $row;
+//        echo '<pre>';
+//        print_r($row);
+//        echo '</pre>';
+
+    }
+
+    foreach ($signLoginCheck as $siItem) {
+
+
+        if ($signLogin == $siItem['login']) {
+            //sleep(5);
+            ?><html>
+
+<head>
+    <meta charset="utf-8">
+    <title>table</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link href="css/style.css" rel="stylesheet">
+    <script src="js/jquery-3.3.1.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/custom.js"></script>
+
+</head>
+<body>
+<div class="form-row align-items-center formDivLogin">
+    <pre>
+    <form action="signup.php" method="post">
+        <input class="form-control" placeholder="Enter nickname" autocomplete="off" name="signName">
+        <input class="form-control" placeholder="Enter login" name="signLogin">
+        <p style="color: red;">Человек с таким логином уже есть</p>
+        <input class="form-control" placeholder="Enter password" type="password" autocomplete="off" name="signPass">
+
+
+        <button class="btn btn-primary" type="submit">Sign UP!</button>
+
+    </form>
+
+
+</div>
+</body>
+</html><?
+
+            exit();
+
+        }
+    }
+
     $currDate = time();
     $signHashPass = password_hash($signPass, PASSWORD_DEFAULT);
     $res = mysqli_query($db,
@@ -38,6 +94,7 @@ if(!empty($signLogin) && !empty($signPass) && !empty($signName) ) {
 
     mysqli_close($db);
     header('location: login.php');
+
 }
 /////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -64,14 +121,14 @@ if(!empty($signLogin) && !empty($signPass) && !empty($signName) ) {
 </head>
 <body>
 <div class="form-row align-items-center formDivLogin">
-    <?='<pre>'?>
+    <?= '<pre>' ?>
     <form action="signup.php" method="post">
         <input class="form-control" placeholder="Enter nickname" autocomplete="off" name="signName">
         <input class="form-control" placeholder="Enter login" name="signLogin">
         <input class="form-control" placeholder="Enter password" type="password" autocomplete="off" name="signPass">
 
 
-        <button class="btn btn-primary"  type="submit">Sign UP!</button>
+        <button class="btn btn-primary" type="submit">Sign UP!</button>
 
     </form>
 
